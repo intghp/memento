ba2d152508601aa13a81ed1fbc964051213285fe
intghp <gustavohlucenapaiva@gmail.com>
2026-08-15 12:08:32 -0300

    feat(models): add Notes models

--- backend/models.py
+++ backend/models.py
@@ -2,6 +2,8 @@ from typing import Optional
 from sqlmodel import Field, SQLModel
 from datetime import datetime, timezone
 
+# --- TAKS MODELS ---
+
 class TaskBase(SQLModel):
     title: str
     description: Optional[str] = None
@@ -15,3 +17,22 @@ class Task(TaskBase, table=True):
 
 class TaskCreate(TaskBase):
     pass
+
+# --- NOTES MODELS ---
+class NoteBase(SQLModel):
+    title: str
+    content: str = ""
+
+class Note(NoteBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    updated_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column_kwargs={"onupdate": lambda: datetime.now(timezone.utc)},
+        )
+
+class NoteCreate(NoteBase):
+    pass
+
+class NoteUpdate(SQLModel):
+    title: Optional[str] = None
+    content: Optional[str] = None
